c109b0dea10fc252845bd8775be94e3056727ae5
David Patnode <draknor@gmail.com>
2015-05-28 21:46:02 -0500

    remove extraneous phases

--- app/models/migration_run.rb
+++ app/models/migration_run.rb
@@ -1,7 +1,7 @@
 class MigrationRun < ActiveRecord::Base
 
   enum status: [ :created, :preparing, :running, :completed_success, :completed_error, :canceled, :unknown, :queued, :aborted, :failed ]
-  enum phase: [:test_only, :create_shell, :add_dependencies, :add_history]
+  enum phase: [:test_only, :create_shell]
   before_save :update_max_records
 
   belongs_to :user
