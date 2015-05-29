7ed296da4ff5c71eebc260d00578fa4604eecfa8
David Patnode <draknor@gmail.com>
2015-05-28 21:49:59 -0500

    changed my mind - need to keep history phase

--- app/models/migration_run.rb
+++ app/models/migration_run.rb
@@ -1,7 +1,7 @@
 class MigrationRun < ActiveRecord::Base
 
   enum status: [ :created, :preparing, :running, :completed_success, :completed_error, :canceled, :unknown, :queued, :aborted, :failed ]
-  enum phase: [:test_only, :create_shell]
+  enum phase: [:test_only, :create_shell, :add_history]
   before_save :update_max_records
 
   belongs_to :user
